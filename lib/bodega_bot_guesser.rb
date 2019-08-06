ea5da09dc5cff43fc950b6fddd815336c4a8d4ce
Zach Margolis <zbmargolis@gmail.com>
2019-08-06 11:24:40 -0700

    Add main check-tweets task

--- lib/bodega_bot_guesser.rb
+++ lib/bodega_bot_guesser.rb
@@ -1,5 +1,13 @@
+require 'logger'
+
 module BodegaBotGuesser
   def self.root
     File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  end
+
+  LOGGER = Logger.new(STDOUT)
+
+  def self.logger
+    LOGGER
   end
 end
