576a252b65b1ceb27d7ef5202516875665da493b
Zach Margolis <zbmargolis@gmail.com>
2019-08-06 17:05:41 -0700

    Update log formatting

--- lib/bodega_bot_guesser.rb
+++ lib/bodega_bot_guesser.rb
@@ -5,7 +5,12 @@ module BodegaBotGuesser
     File.expand_path(File.join(File.dirname(__FILE__), '..'))
   end
 
-  LOGGER = Logger.new(STDOUT)
+  LOGGER = Logger.new(STDOUT).tap do |logger|
+    # heroku already captures the timestamp for log messages
+    logger.formatter = proc do |severity, _datetime, _progname, msg|
+      "#{severity} -- #{msg}\n"
+    end
+  end
 
   def self.logger
     LOGGER
